9d3b6c9404b6c85d3085a10c4d9221dd0809db2b
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-10-02 23:50:38 +0200

    .

--- addressbook.h
+++ addressbook.h
@@ -10,16 +10,18 @@ class QWidget;
 class AddressBook
 {
 	Q_DECLARE_TR_FUNCTIONS(AddressBook)
-	
+
 public:
+	AddressBook();
+
 	bool contains(const QString &name) const;
 	QHostAddress address(const QString &name) const;
-	
+
 	void add(const QString &name, const QHostAddress &address);
 	void remove(const QString &name);
-	
+
 	static void showAddressDialog(QWidget *parent = nullptr);
-	
+
 private:
 	QHash<QString, QHostAddress> _data;
 };
